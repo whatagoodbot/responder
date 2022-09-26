9b8ba28681494876f0abd594c77620e518f94754
notABluesSinger <rob@whatagoodbot.com>
2022-09-26 09:34:28 +0100

    Lint fixes

--- src/libs/metrics.js
+++ src/libs/metrics.js
@@ -2,11 +2,11 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const StatsD = require('hot-shots')
 
-const  client = new StatsD({
+const client = new StatsD({
   host: '192.168.4.56',
   globalTags: { bucket: 'goodbot', system: 'responder', env: process.env.NODE_ENV },
   telegraf: true
 })
 
-  // Increment: Increments a stat by a value (default is 1)
-  client.increment('meeting',['room:rock'])
+// Increment: Increments a stat by a value (default is 1)
+client.increment('meeting', ['room:rock'])
