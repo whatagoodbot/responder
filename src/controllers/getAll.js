567c2a7599d78d6d2fc983b3df29cba196060ff5
notABluesSinger <rob@whatagoodbot.com>
2022-09-26 11:19:11 +0100

    Basic metrics

--- src/controllers/getAll.js
+++ src/controllers/getAll.js
@@ -1,5 +1,7 @@
 import { responsesDb } from '../models/index.js'
+import { metrics } from '../libs/metrics.js'
 
 export default async (payload) => {
+  metrics.count('getAllResponses', { room: payload.room })
   return await responsesDb.getAll(payload.room)
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import { connect } from 'mqtt'
 import { logger } from './utils/logging.js'
 import mqttOptions from './config.js'
 import { topics, topicPrefix } from './topics.js'
+import { metrics } from './libs/metrics.js'
+import { performance } from 'perf_hooks'
+
 const client = connect(mqttOptions.host, mqttOptions)
 
 client.on('connect', () => {
@@ -14,13 +17,15 @@ client.on('connect', () => {
 })
 
 client.on('message', async (topic, message) => {
+  const startTime = performance.now()
   try {
+    metrics.count('receivedMessage', { topic })
     const request = JSON.parse(message.toString())
     const response = await topics[topic.substring(topicPrefix.length)].responder(request)
+    const endTime = performance.now()
+    metrics.timer('responseTime', endTime - startTime, { topic })
     client.publish(topics[topic.substring(topicPrefix.length)].replyTopic, JSON.stringify({ request, response }))
   } catch (error) {
     client.publish(topics[topic.substring(topicPrefix.length)].replyTopic, JSON.stringify({ error: error.toString() }))
   }
 })
-
-logger.info(process.env.npm_package_version)
