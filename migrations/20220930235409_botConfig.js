d862ea2bbf2a7b08af036066361a1f3a7581030c
notABluesSinger <rob@whatagoodbot.com>
2022-10-01 01:34:38 +0100

    Fixed Linting

--- migrations/20220930235409_botConfig.js
+++ migrations/20220930235409_botConfig.js
@@ -1,11 +1,11 @@
 
 export const up = (knex) => {
   return knex.schema
-  .createTable('config', function (table) {
-    table.string('name', 255).notNullable().primary()
-    table.json('config')
-    table.timestamps(true, true, true)
-  })
+    .createTable('config', function (table) {
+      table.string('name', 255).notNullable().primary()
+      table.json('config')
+      table.timestamps(true, true, true)
+    })
 }
 
 export const down = (knex) => {
