907c0f2cec41ef7d40cbb1bff5bc4a45e00b643e
notABluesSinger <rob@whatagoodbot.com>
2022-10-01 01:32:38 +0100

    Migrations for bot sentience and bot config

---
+++ migrations/20220930235409_botConfig.js
@@ -0,0 +1,14 @@
+
+export const up = (knex) => {
+  return knex.schema
+  .createTable('config', function (table) {
+    table.string('name', 255).notNullable().primary()
+    table.json('config')
+    table.timestamps(true, true, true)
+  })
+}
+
+export const down = (knex) => {
+  return knex.schema
+    .dropTable('config')
+}
