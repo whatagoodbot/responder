957b70acc9f166240dc9766b2e2436a0edb584f5
notABluesSinger <rob@whatagoodbot.com>
2022-10-02 17:04:24 +0100

    Applying new format for schemas

---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+import getResponse from './getResponse.js'
+import getAllResponses from './getAllResponses.js'
+import addResponse from './addResponse.js'
+
+export default {
+  getResponse,
+  getAllResponses,
+  addResponse
+}
