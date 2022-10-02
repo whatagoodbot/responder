03ae6b3e4cf585f83313074e1cb61a429ac88203
notABluesSinger <rob@whatagoodbot.com>
2022-10-02 15:01:27 +0100

    Dont need the config here

--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,9 @@ import knexfile from '../../knexfile.js'
 
 import responsesModel from './responses.js'
 import stringsModel from './strings.js'
-import configModel from './config.js'
 
 const require = createRequire(import.meta.url)
 const { knex } = require('../libs/knex.cjs')(knexfile[process.env.NODE_ENV])
 
 export const responsesDb = responsesModel(knex)
 export const stringsDb = stringsModel(knex)
-export const configDb = configModel(knex)
