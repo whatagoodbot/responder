1c2fab5c231d500d7f5847618d2d9d30f1a72d05
notABluesSinger <rob@whatagoodbot.com>
2022-10-05 17:22:51 +0100

    Schema for other services to request broadcast

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,11 +3,13 @@ import responseAdd from './responseAdd.js'
 import responseRead from './responseRead.js'
 import responseReadAll from './responseReadAll.js'
 import songPlayed from './songPlayed.js'
+import requestToBroadcast from './requestToBroadcast.js'
 
 export default {
   chatMessage,
   responseAdd,
   responseRead,
   responseReadAll,
-  songPlayed
+  songPlayed,
+  requestToBroadcast
 }
---
+++ src/controllers/requestToBroadcast.js
@@ -0,0 +1,6 @@
+import { metrics } from '../utils/metrics.js'
+
+export default async (payload) => {
+  metrics.count('requestToBroadcast', payload)
+  return payload
+}
