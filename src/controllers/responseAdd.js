a4056c826f012fb46c43a27579389bd80e256f2e
notABluesSinger <rob@whatagoodbot.com>
2022-10-06 11:08:46 +0100

    correct responses added & aliases listed message

--- src/controllers/responseAdd.js
+++ src/controllers/responseAdd.js
@@ -1,11 +1,12 @@
 import { responsesDb } from '../models/index.js'
 import { metrics } from '../utils/metrics.js'
+import getRandomString from '../utils/getRandomString.js'
 
 export default async (payload) => {
   metrics.count('addResponse', payload)
   const result = await responsesDb.add(payload.key, payload.room, payload.type, payload.value, payload.category)
   if (result) {
-    const intro = await responsesDb.get(null, 'aliasAdded', 'system')
+    const intro = getRandomString(await responsesDb.get(null, `${payload.category}Added`, 'system'))
     return {
       message: `${intro.value} ${payload.key}`
     }
--- src/controllers/responseReadAll.js
+++ src/controllers/responseReadAll.js
@@ -1,10 +1,12 @@
 import { responsesDb } from '../models/index.js'
 import { metrics } from '../utils/metrics.js'
+import getRandomString from '../utils/getRandomString.js'
 
 export default async (payload) => {
   metrics.count('getAllResponses', payload)
   const replies = await responsesDb.getAll(payload.room, payload.category)
-  const intro = await responsesDb.get(null, 'aliasIntro', 'system')
+  const intro = getRandomString(await responsesDb.get(null, 'aliasIntro', 'system'))
+
   return {
     message: `${intro.value} ${replies.map((reply) => {
       return reply.name
