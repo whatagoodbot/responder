4b22d4137c55ee05d9cabbbc7dad5d013377a269
notABluesSinger <rob@whatagoodbot.com>
2022-10-29 22:15:59 +0100

    use env var for server address

--- src/libs/grpc.js
+++ src/libs/grpc.js
@@ -1,6 +1,6 @@
 import { clientCreds, users } from '@whatagoodbot/rpc'
 
-const userService = new users.Users('localhost:50051', clientCreds)
+const userService = new users.Users(`${process.env.USERS_SERVICE}:50051`, clientCreds)
 
 export const getUser = id => {
   return new Promise(resolve => {
