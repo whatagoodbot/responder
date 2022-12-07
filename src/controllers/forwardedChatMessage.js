171deb3df3b2c90a1da6ef769de6a7fa49706693
Rob Johnson <rob@whatagoodbot.com>
2022-12-07 14:32:56 +0000

    Update forwardedChatMessage.js

--- src/controllers/forwardedChatMessage.js
+++ src/controllers/forwardedChatMessage.js
@@ -67,7 +67,7 @@ export default async payload => {
           returnPayload = {
             topic: 'broadcast',
             payload: {
-              message: getRandom.fromArray(matchString.value)
+              message: getRandom.fromArray(matchString.value.split(',')
             }
           }
         }
